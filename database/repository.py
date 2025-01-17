7f6b79fc390a1c51489ef8f58734b819db95440f
Vitalya <vitamin11182005@gmail.com>
2025-01-17 19:40:07 +0400

    encapsulate sql queries

---
+++ database/repository.py
@@ -0,0 +1,80 @@
+from database.postgres import Database
+import utils.config as config
+from datetime import datetime
+
+cfg = config.load()
+
+db = Database(cfg)
+
+async def read_parameter(user_id, field):
+    res = await db.fetch(f'SELECT {field} FROM users WHERE user_id = $1', user_id)
+    if res:
+        return res[0][field]
+    return None
+
+async def update_parameter(user_id, field, value):
+    query = f"UPDATE users SET {field} = $1 WHERE user_id = $2"
+    await db.execute(query, value, user_id)
+
+async def add_user(user_id, name, mob_number, age, gender, height, weight, timezone):
+    query = """
+    INSERT INTO users (user_id, name, mob_number, age, gender, height, weight, timezone)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+    """
+    await db.execute(query, user_id, name, mob_number, age, gender, height, weight, timezone)
+
+async def get_user(user_id):
+    query = """
+    SELECT * FROM users WHERE user_id = $1
+    """
+    result = await db.fetch(query, user_id)
+    return result
+
+## убрать и воспользоваться гет юзер
+async def if_exists(user_id):
+    query = """
+    SELECT EXISTS (
+        SELECT 1 
+        FROM users
+        WHERE user_id = $1
+    )
+    """
+    result = await db.fetch(query, user_id)
+    return result[0]['exists']
+
+async def get_weight(user_id):
+    query = """
+    SELECT weight
+    FROM users
+    WHERE user_id = $1
+    """
+    result = await db.fetch(query, user_id)
+    return result[0]['weight']
+
+async def add_water(user_id):
+    current_day = datetime.now().date()
+    res = await get_water(db, user_id, current_day)
+    if res:
+        new_water_amount = res + 250
+        await db.execute(
+            'UPDATE user_drinked_water SET water = $1 WHERE user_id = $2 AND day = $3',
+            new_water_amount, user_id, current_day
+        )
+    else:
+        await db.execute(
+            'INSERT INTO user_drinked_water (user_id, day, water) VALUES ($1, $2, $3)',
+            user_id, current_day, 250
+        )
+
+async def get_water(user_id, day):
+    res = await db.fetch('SELECT water FROM user_drinked_water WHERE user_id = $1 AND day = $2', user_id, day)
+    if res:
+        return res[0]['water']
+    return None
+
+async def save_survey_data(user_id, data):
+    query = """
+    INSERT INTO daily_survey (user_id, survey_date, physical_activity, stress, mood, sleep_quality)
+    VALUES ($1, $2, $3, $4, $5, $6)
+    """
+    await db.execute(query, user_id, data['survey_date'], data['physical_activity'], data['stress'], data['mood'], data['sleep_quality'])
---
+++ handlers/profile.py
@@ -0,0 +1,118 @@
+from handlers.handler import router
+from database import repository
+
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.filters import Command
+from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import CallbackQuery
+
+class EditProfile(StatesGroup):
+    age = State()
+    height = State()
+    weight = State()
+
+edit_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Изменить возраст", callback_data="edit_age"),
+        ],
+        [
+            InlineKeyboardButton(text="Изменить рост", callback_data="edit_height"),
+        ],
+        [
+            InlineKeyboardButton(text="Изменить вес", callback_data="edit_weight"),
+        ]
+    ]
+)
+
+@router.message(Command('profile'))
+async def show_profile(message: Message):
+    user_id = message.from_user.id
+
+    user_data = await repository.get_user(user_id)
+
+    if not user_data:
+        await message.answer("Ваш профиль пуст. Пожалуйста, пройдите регистрацию командой /registration.")
+        return
+    user_data = user_data[0]
+    profile_text = (
+        f"Ваш профиль:\n"
+        f"Имя: {user_data['name'] or 'Не указано'}\n"
+        f"Номер телефона: {user_data['mob_number'] or 'Не указано'}\n"
+        f"Возраст: {user_data['age'] or 'Не указано'}\n"
+        f"Пол: {user_data['gender'] or 'Не указано'}\n"
+        f"Рост: {user_data['height'] or 'Не указано'}\n"
+        f"Вес: {user_data['weight'] or 'Не указано'}\n\n"
+        f"Если хотите изменить данные, напишите команду /edit и выберите, что изменить."
+    )
+
+    await message.answer(profile_text)
+
+
+@router.message(Command('edit'))
+async def edit_profile(message: Message):
+    await message.answer("Выберите, что вы хотите изменить:", reply_markup=edit_keyboard)
+
+@router.callback_query(lambda c: c.data == "edit_age")
+async def edit_age(callback: CallbackQuery, state: FSMContext):
+    await callback.message.answer("Введите новый возраст:")
+    await state.set_state(EditProfile.age)
+    await callback.answer()
+
+@router.message(EditProfile.age)
+async def update_age(message: Message, state: FSMContext):
+    if not message.text.isdigit() or int(message.text) <= 0:
+        await message.answer("Пожалуйста, введите корректный возраст (число).")
+        return
+
+    new_age = int(message.text)
+    user_id = message.from_user.id
+
+    await repository.update_parameter(user_id, "age", new_age)
+
+    await message.answer("Ваш возраст успешно обновлён.")
+    await state.clear()
+
+
+@router.callback_query(lambda c: c.data == "edit_height")
+async def edit_height(callback: CallbackQuery, state: FSMContext):
+    await callback.message.answer("Введите новый рост (в сантиметрах):")
+    await state.set_state(EditProfile.height)  # Устанавливаем состояние для редактирования роста
+    await callback.answer()
+
+@router.message(EditProfile.height)
+async def update_height(message: Message, state: FSMContext):
+    if not message.text.isdigit() or int(message.text) <= 0:
+        await message.answer("Пожалуйста, введите корректный рост (число).")
+        return
+
+    new_height = int(message.text)
+    user_id = message.from_user.id
+
+    await repository.update_parameter(user_id, "height", new_height)
+
+    await message.answer("Ваш рост успешно обновлён.")
+    await state.clear()
+
+
+@router.callback_query(lambda c: c.data == "edit_weight")
+async def edit_weight(callback: CallbackQuery, state: FSMContext):
+    await callback.message.answer("Введите новый вес (в килограммах):")
+    await state.set_state(EditProfile.weight) 
+    await callback.answer()
+
+@router.message(EditProfile.weight)
+async def update_weight(message: Message, state: FSMContext):
+    if not message.text.isdigit() or int(message.text) <= 0:
+        await message.answer("Пожалуйста, введите корректный вес (число).")
+        return
+
+    new_weight = int(message.text)
+    user_id = message.from_user.id
+
+    await repository.update_parameter(user_id, "weight", new_weight)
+
+    await message.answer("Ваш вес успешно обновлён.")
+    await state.clear()
