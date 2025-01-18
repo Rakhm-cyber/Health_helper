61a6bddc78a5d0419341c6af12ed500f3ed1eecf
Vitalya <vitamin11182005@gmail.com>
2025-01-18 13:13:54 +0400

    add get weekly survey data method

--- database/repository.py
+++ database/repository.py
@@ -30,18 +30,6 @@ async def get_user(user_id):
     result = await db.fetch(query, user_id)
     return result
 
-## убрать и воспользоваться гет юзер
-async def if_exists(user_id):
-    query = """
-    SELECT EXISTS (
-        SELECT 1 
-        FROM users
-        WHERE user_id = $1
-    )
-    """
-    result = await db.fetch(query, user_id)
-    return result[0]['exists']
-
 async def get_weight(user_id):
     query = """
     SELECT weight
@@ -78,6 +66,18 @@ async def save_survey_data(user_id, data):
     VALUES ($1, $2, $3, $4, $5, $6)
     """
     await db.execute(query, user_id, data['survey_date'], data['physical_activity'], data['stress'], data['mood'], data['sleep_quality'])
+
+async def get_weekly_survey_data(user_id, field):
+    query = f"""
+    SELECT survey_date, {field} FROM daily_survey
+    WHERE user_id = $1 AND survey_date >= now() - interval '7 days'
+    ORDER BY survey_date ASC
+    """
+    data = await db.fetch(query, user_id)
+    if data:
+        return data
+    return None
+
 
 async def save_action(user_id, username, action_type, message, timestamp):
     query = """
